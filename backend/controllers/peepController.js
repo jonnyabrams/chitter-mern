b149e0cd7a32fd934d136102dd2d72a50b384296
Jonny Abrams <jonnyabrams@hotmail.com>
2022-05-25 17:31:15 +0100

    Added middleware to server.js

--- backend/controllers/peepController.js
+++ backend/controllers/peepController.js
@@ -1,3 +1,5 @@
+const { restart } = require("nodemon")
+
 //route: GET /api/goals
 const getPeeps = (req, res) => {
   res.status(200).json({ message: 'Get peeps' })
@@ -5,17 +7,21 @@ const getPeeps = (req, res) => {
 
 //route: POST /api/goals
 const setPeep = (req, res) => {
+  if(!req.body.text) {
+    res.status(400)
+    throw new Error('Please write something')
+  }
   res.status(200).json({ message: 'Set peep' })
 }
 
 //route: PUT /api/goals/:id
 const updatePeep = (req, res) => {
-  res.status(200).json({message: `Update peep ${req.params.id}`})
+  res.status(200).json({ message: `Update peep ${req.params.id}` })
 }
 
 //route: DELETE /api/goals/:id
 const deletePeep = (req, res) => {
-  res.status(200).json({message: `Delete peep ${req.params.id}`})
+  res.status(200).json({ message: `Delete peep ${req.params.id}` })
 }
 
 
