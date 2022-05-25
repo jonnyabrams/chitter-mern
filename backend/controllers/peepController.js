98291adeccab886b32551e6a3d78485f725f6520
Jonny Abrams <jonnyabrams@hotmail.com>
2022-05-25 19:05:19 +0100

    Installed asyncHandler and added it to peepController

--- backend/controllers/peepController.js
+++ backend/controllers/peepController.js
@@ -1,28 +1,29 @@
 const { restart } = require("nodemon")
+const asyncHandler = require('express-async-handler')
 
 //route: GET /api/goals
-const getPeeps = (req, res) => {
+const getPeeps = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get peeps' })
-}
+})
 
 //route: POST /api/goals
-const setPeep = (req, res) => {
+const setPeep = asyncHandler(async (req, res) => {
   if(!req.body.text) {
     res.status(400)
     throw new Error('Please write something')
   }
   res.status(200).json({ message: 'Set peep' })
-}
+})
 
 //route: PUT /api/goals/:id
-const updatePeep = (req, res) => {
+const updatePeep = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update peep ${req.params.id}` })
-}
+})
 
 //route: DELETE /api/goals/:id
-const deletePeep = (req, res) => {
+const deletePeep = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete peep ${req.params.id}` })
-}
+})
 
 
 module.exports = {
